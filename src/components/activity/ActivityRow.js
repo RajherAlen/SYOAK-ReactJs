7acfff39a62dcb1994a2b55ed616acfe9f7fb708
Alen <alen.rajher@gmail.com>
2020-09-12 11:53:16 +0200

    izmijenio Activity

--- src/components/activity/ActivityRow.js
+++ src/components/activity/ActivityRow.js
@@ -1,26 +1,27 @@
 import React from "react";
-import { Avatar } from "@material-ui/core";
 import path from "./path.svg";
 
 const ActivityRow = ({ time, title, name, text }) => {
   return (
     <div className="activity__row">
-      <p className="activity__row__info activity__row__info--bold"> {time} </p>
-      <Avatar className="avatar">
+      <p className="activity__row__info activity__row__info--bold activity__row__info--bold--dark">
         {" "}
-        <img src={path} />{" "}
-      </Avatar>
+        {time}{" "}
+      </p>
+      <img src={path} />
+
       <div className="activity__row__wrap">
-        <p className="activity__row__info activity__row__info--bold activity__row__info--bold-dark">
+        <p className="activity__row__info activity__row__info--bold activity__row__info--bold--dark">
           {" "}
           {title}{" "}
         </p>
-        <p className="activity__row__info activity__row__info--light activity__row__info--light-grey">
+        <p className="activity__row__info activity__row__info--light activity__row__info--light--grey">
           {" "}
           {name}{" "}
         </p>
       </div>
-      <p className="activity__row__info activity__row__info--bold activity__row__info--bold-grey">
+
+      <p className="activity__row__info activity__row__info--bold activity__row__info--bold--grey">
         {" "}
         {text}{" "}
       </p>
